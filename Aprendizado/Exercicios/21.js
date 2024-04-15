d3d9cd62ea6b5d3af9681791c0d0eb4ed4f6389d
James Marques <Jamzmarks@gmail.com>
2024-04-14 22:47:44 -0300

    Exercico 21

--- Aprendizado/Exercicios/21.js
+++ Aprendizado/Exercicios/21.js
@@ -7,28 +7,25 @@ class PlanoSaude{
         this.sexo = sexo
     }
     precoPlano(){
-        const precoIdade = IddPrecoPlano(this)
+        const precoIdade = IddPrecoPlano(this.idade)
         const total = precoPlano + precoIdade
         return total
     }
 }
 
-const p1 = new PlanoSaude('Fernando', 49, 'masculino')
-console.log(p1)
-
-
-function IddPrecoPlano(conveniado){
-    switch(conveniado){
-        case this.idade < 10:
-            return 80
-        case this.idade >= 10 && this.idade < 30:
-            return 50
-        case this.idade >= 30 && this.idade < 60:
-            return 95
-        case this.idade >= 60:
-            return 130
+function IddPrecoPlano(idade){
+    if(idade < 10){
+        return 80;
+    }else if(idade >= 10 && idade < 30){
+        return 50;
+    }else if(idade >= 30 && idade < 60){
+        return 95;
+    }else if(idade >= 60){
+        return 130;
     }
 }
+
+const p1 = new PlanoSaude('Fernando', 48, 'masculino')
 
 const msg = p1.precoPlano()
 console.log(`O preco total do plano do Sr(a)${p1.nome} sera ${msg} `)
