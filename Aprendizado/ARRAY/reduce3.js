24d162af9e9926a84d98facbccbed800bc354685
James Marques <Jamzmarks@gmail.com>
2024-04-17 23:25:01 -0300

    reduce 3

---
+++ Aprendizado/ARRAY/reduce3.js
@@ -0,0 +1,19 @@
+Array.prototype.reduce2 = function(callback, valorInicial){
+    const indiceInicial = valorInicial ? 0 : 1
+    let acumulador = valorInicial || this[0]
+    for(let i = indiceInicial; i < this.length; i++){
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+    return acumulador
+}
+
+const alunos = [
+    {nome: 'Joao', nota: 7.3, bolsista: true},
+    {nome: 'Maria', nota: 9.2, bolsista: true},
+    {nome: 'Pedro', nota: 9.8, bolsista: true},
+    {nome: 'Ana', nota: 8.7, bolsista: true},
+]
+
+const tdBolsistas = alunos.reduce2((resultado, aluno) => resultado && aluno.bolsista)
+
+console.log(tdBolsistas)
