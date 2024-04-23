68ee14eef70b207c4013c3877563d726d89c389a
James Marques <Jamzmarks@gmail.com>
2024-04-23 00:34:34 -0300

    Delete - Update - Read - Create

--- Aprendizado/NODE/Projeto/src/servidor.js
+++ Aprendizado/NODE/Projeto/src/servidor.js
@@ -25,6 +25,19 @@ app.post('/produtos', (req, res, next) =>{
     res.send(produto) //JSON
 })
 
+app.put('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: parseFloat(req.body.preco)
+    })
+    res.send(produto) //JSON
+})
+app.delete('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) //JSON
+})
+
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
 })
