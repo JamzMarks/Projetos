4a48d5635a1016ef027c7853546f1b631206daca
James Marques <Jamzmarks@gmail.com>
2024-05-03 16:25:58 -0300

    att

--- Aprendizado/ESNext/taggedTemplate2.js
+++ Aprendizado/ESNext/taggedTemplate2.js
@@ -9,6 +9,6 @@ function real(partes, ...valores){
     return resultado.join('')
 }
 
-const preco = 29.99
+const preco = 29.98
 const precoParcela = 11
 console.log(real`1x de ${preco} ou 3x de ${precoParcela}.`)
