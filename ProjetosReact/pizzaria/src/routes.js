8320e95636b51013e4c2d11b9d4651542bfa3d3d
James Marques <Jamzmarks@gmail.com>
2024-09-01 23:14:06 -0300

    Att

---
+++ ProjetosReact/pizzaria/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+
+export default function AppRouter(){
+  return (
+    <main>
+      <Router>
+        <Menu></Menu>
+        <Routes>
+          <Route path='/' element={<PaginaPadrao/>}>
+            <Route index element={<Inicio/>}/>
+            <Route path='cardapio' element={<Cardapio/>}/>
+          </Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+}
